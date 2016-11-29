076958ed84ee1795e2655686503f39738f5bbb31
Michael Rüegg <rueegg.michael@gmail.com>
2016-11-29 09:29:00 +0100

    updated DSL to version 1.3.0

--- dsls/HelloWorldPlan.groovy
+++ dsls/HelloWorldPlan.groovy
@@ -1,21 +1,16 @@
-project("MYPROJECT") {
-    name "My project"
-
-    plan("MYPLAN") {
-        name "My plan"
-
-        stage("My stage") {
-            description "My stage"
+project(key: 'MYPROJECT', name: 'My project') {
+    plan(key: 'MYPLAN', name: 'My plan') {
+        stage(name: 'My stage') {
+            description 'My stage'
             manual false
 
-            job("BUILD") {
-                name "Maven build job"
-
+            job(key: 'BUILD', name: 'Maven build job' ) {
                 tasks {
-                    maven3("build plug-in") {
-                        goal "install"
-                        executable "maven323"
-                        buildJdk "jdk8"
+                    maven3x(goal: 'install') {
+                        description 'build plug-in'
+                        goal 'install'
+                        executable 'maven323'
+                        buildJdk 'jdk8'
                         environmentVariables 'MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=128m"'
                     }
                 }

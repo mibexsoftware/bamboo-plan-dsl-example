e1a3438f911fabc39b5c96db584d3eee2368b805
Michael Rüegg <rueegg.michael@gmail.com>
2017-02-17 10:11:01 +0100

    Changed from Maven task in example DSL script to script task

--- dsls/HelloWorldPlan.groovy
+++ dsls/HelloWorldPlan.groovy
@@ -6,11 +6,12 @@ project(key: 'MYPROJECT', name: 'My project') {
 
             job(key: 'BUILD', name: 'Maven build job' ) {
                 tasks {
-                    maven3x(goal: 'install') {
-                        description 'build plug-in'
-                        executable 'maven323'
-                        buildJdk 'jdk8'
-                        environmentVariables 'MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=128m"'
+                    script() {
+                        description 'echo hello world'
+                        inline {
+                            interpreter ScriptInterpreter.LEGACY_SH_BAT
+                            scriptBody 'echo "Hello World"'
+                        }
                     }
                 }
             }

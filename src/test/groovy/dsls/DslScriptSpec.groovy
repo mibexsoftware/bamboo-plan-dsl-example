1c2fc939e1656a66906b38551ab39cb99dc0ebe5
Michael Rüegg <rueegg.michael@gmail.com>
2017-06-19 10:43:45 +0200

    Test refactorings

--- src/test/groovy/dsls/DslScriptSpec.groovy
+++ src/test/groovy/dsls/DslScriptSpec.groovy
@@ -2,6 +2,7 @@ package dsls
 
 import ch.mibex.bamboo.plandsl.dsl.DslScriptContext
 import ch.mibex.bamboo.plandsl.dsl.DslScriptParserImpl
+import ch.mibex.bamboo.plandsl.dsl.ScriptLanguage
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -13,8 +14,8 @@ class DslScriptSpec extends Specification {
 
         when:
         def loader = new DslScriptParserImpl()
-        def additionalClassPath = new File('src/main/groovy').toURI().toURL()
-        loader.parse(new DslScriptContext(file.toURI().toString(), null, additionalClassPath))
+        def classPath = new File('src/main/groovy').toURI().toURL()
+        loader.parse(new DslScriptContext(file.toURI().toString(), null, ScriptLanguage.GROOVY_DSL, classPath))
 
         then:
         noExceptionThrown()
